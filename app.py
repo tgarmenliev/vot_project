edca86c8e4615cc423b4f14e5e6b79f0b48ecfec
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2023-04-12 12:06:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,91 @@
 from flask import Flask, render_template, request
-import pymysql.cursors
 import requests
-from datetime import datetime, timedelta
-import threading
 import time
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='fluser',
-                             password='Password123#@!',
-                             db='flapp',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+# connect to the MySQL database
+db = pymysql.connect(
+    host='localhost',
+    user='fluser',
+    password='Password123#@!',
+    database='flapp'
+)
 
+cursor = db.cursor()
+
+# create a table to store website URLs and bounce counts
+sql = '''CREATE TABLE IF NOT EXISTS urls (
+         id INT(11) NOT NULL AUTO_INCREMENT,
+         url VARCHAR(255) NOT NULL,
+         bounces INT(11) NOT NULL DEFAULT '0',
+         last_online TIMESTAMP DEFAULT NULL,
+         PRIMARY KEY (id)
+      ) ENGINE=InnoDB AUTO_INCREMENT=1'''
+cursor.execute(sql)
+
+
+# function to check if a website is online
 def is_online(url):
     try:
         response = requests.get(url)
-        return response.status_code == 200
-    except:
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException:
         return False
 
-def track_bounces():
-    while True:
-        with connection.cursor() as cursor:
-            sql = "SELECT `url` FROM `urls`"
-            cursor.execute(sql)
-            urls = cursor.fetchall()
-            for url in urls:
-                url = url['url']
-                if is_online(url):
-                    sql = "UPDATE `urls` SET `last_checked`=%s, `bounces`=0 WHERE `url`=%s"
-                    current_time = datetime.now()
-                    cursor.execute(sql, (datetime.now(), url))
-                    connection.commit()
-                else:
-                    sql = "SELECT `bounces` FROM `urls` WHERE `url`=%s"
-                    cursor.execute(sql, url)
-                    bounces = cursor.fetchone()['bounces']
-                    if bounces < 2:
-                        sql = "UPDATE `urls` SET `bounces`=%s WHERE `url`=%s"
-                        cursor.execute(sql, (bounces + 1, url))
-                        connection.commit()
-                    else:
-                        sql = "UPDATE `urls` SET `bounces`=%s WHERE `url`=%s"
-                        cursor.execute(sql, (bounces + 1, url))
-                        connection.commit()
-                        send_notification(url)
-
-        # Wait for 10 minutes before checking again
-        time.sleep(5)
-
-def send_notification(url):
-    # Code to send notification (e.g. email) goes here
-    print(f"{url} is down!")
-
+# home page route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# add URL route
 @app.route('/add_url', methods=['POST'])
 def add_url():
     url = request.form['url']
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO `urls` (`url`, `last_checked`, `bounces`) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (url, datetime.now(), 0))
-        connection.commit()
-    return render_template('index.html', message="URL added successfully")
+    # insert the new URL into the database
+    sql = "INSERT INTO urls (url) VALUES (%s)"
+    cursor.execute(sql, (url,))
+    db.commit()
+    return render_template('index.html')
 
+# status page route
 @app.route('/status')
 def status():
-    with connection.cursor() as cursor:
-        sql = "SELECT `url`, `bounces`, `last_checked` FROM `urls`"
+    # select all URLs and their bounce counts from the database
+    sql = "SELECT url, bounces FROM urls"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    bounces = {result[0]: result[1] for result in results}
+    return render_template('status.html', bounces=bounces, is_online=is_online)
+
+# function to track bounces and last online status
+def track_bounces():
+    while True:
+        # select all URLs from the database
+        sql = "SELECT url FROM urls"
         cursor.execute(sql)
-        bounces = cursor.fetchall()
-    return render_template('status.html', bounces=bounces)
+        results = cursor.fetchall()
+        urls = [result[0] for result in results]
+        for url in urls:
+            online = is_online(url)
+            # update the bounce count and last online status for the URL in the database
+            if online:
+                sql = "UPDATE urls SET bounces=bounces+1, last_online=NOW() WHERE url=%s"
+            else:
+                sql = "UPDATE urls SET last_online=NOW() WHERE url=%s"
+            cursor.execute(sql, (url,))
+            db.commit()
+        time.sleep(5) # wait for 60 seconds before checking again
 
+
+# start the tracking thread when the application starts
 if __name__ == '__main__':
-    # Start the thread that tracks bounces
-    t = threading.Thread(target=track_bounces)
-    t.start()
-
+    import threading
+    tracking_thread = threading.Thread(target=track_bounces)
+    tracking_thread.start()
     app.run(debug=True)
