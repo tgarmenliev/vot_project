8b7f5ea468de3d8d6235f676277bbde3f9c23fd4
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2023-04-16 12:32:35 +0300

    Create init.sql

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS flapp;
+CREATE DATABASE flapp;
+use flapp;
+
+CREATE TABLE IF NOT EXISTS `urls` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `url` varchar(255) NOT NULL,
+    `last_online` TIMESTAMP,
+    `bounces` int(11) NOT NULL DEFAULT '0',
+    `history` TEXT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `bounce_events` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `url_id` int(11) NOT NULL,
+    `bounce_time` TIMESTAMP DEFAULT NULL,
+    `status` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `url_id_fk` FOREIGN KEY (`url_id`) REFERENCES `urls`(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
